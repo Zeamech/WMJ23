02265a8a2491e32f5397fcbaee9b378dcc041c4b
Brett <brettsantema@gmail.com>
2023-12-28 13:48:31 -0600

    Move Map generator to prefab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public List<List<LocationType>> Map;
-    public List<Ally> Party;
-    //public List<CharmSong> Melodies;
-    public int Gold;
+    public GameObject MapPrefab;
+    [HideInInspector] public Map Map;
+    [HideInInspector] public List<Ally> Party;
+    //[HideInInspector] public List<CharmSong> Melodies;
+    [HideInInspector] public int Gold;
 
     // Start is called before the first frame update
     void Start()
@@ -15,41 +16,14 @@ public class GameManager : MonoBehaviour
         Party = new List<Ally>();
         Gold = 20;
 
-        // Generate Graph
-        Map = new List<List<LocationType>>();
-
-        //Start with a guaranteed creature encounter
-        List<LocationType> firstLayer = new List<LocationType>();
-        firstLayer.Add(LocationType.Creature);
-        firstLayer.Add(LocationType.Creature);
-
-        // Add 20 random layers
-        for (int i = 0; i < 20; i++)
-		{
-            // Generate 1 to 3 random locations
-            List<LocationType> layer = new List<LocationType>();
-            int r = Random.Range(1, 4);
-            for(int j = 0; j < r; j++) layer.Add((LocationType)Random.Range(0,(int)LocationType.NumberOfLocationTypes));
-            Map.Add(layer);
-		}
-
-        // End with a final boss
-        List<LocationType> finalLayer = new List<LocationType>();
-        finalLayer.Add(LocationType.FinalBoss);
+        // Generate Map
+        GameObject MapGO = Instantiate(MapPrefab, transform.position, Quaternion.identity);
+        Map = MapGO.GetComponent<Map>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    public enum LocationType
-    {
-        Creature,
-        Battle,
-        Treasure,
-        Shop,
-        FinalBoss,
-        NumberOfLocationTypes
     }
 }
---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    [HideInInspector] public SpriteRenderer BGSpriteRenderer;
+    public List<List<LocationType>> MapLocations;
+    public void Start()
+	{
+        BGSpriteRenderer = GetComponent<SpriteRenderer>();
+        MapLocations = new List<List<LocationType>>();
+
+        // Start with a guaranteed creature encounter
+        List<LocationType> firstLayer = new List<LocationType>();
+        firstLayer.Add(LocationType.Creature);
+        firstLayer.Add(LocationType.Creature);
+        MapLocations.Add(firstLayer);
+
+        // Add 20 random layers
+        for (int i = 0; i < 20; i++)
+        {
+            // Generate 1 to 3 random locations
+            List<LocationType> layer = new List<LocationType>();
+            int r = Random.Range(1, 4);
+            for (int j = 0; j < r; j++) layer.Add((LocationType)Random.Range(0, (int)LocationType.NumberOfLocationTypes));
+            MapLocations.Add(layer);
+        }
+
+        // End with a final boss
+        List<LocationType> finalLayer = new List<LocationType>();
+        finalLayer.Add(LocationType.FinalBoss);
+        MapLocations.Add(finalLayer);
+    }
+
+    public enum LocationType
+    {
+        Creature,
+        Battle,
+        Treasure,
+        Shop,
+        FinalBoss,
+        NumberOfLocationTypes
+    }
+}
